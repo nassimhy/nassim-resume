a5e7629f4ce6a595cda1fb6b649b8013dd4efb95
Nassim <nassimhy@gmail.com>
2019-09-25 20:44:18 +0330

    Updates

---
+++ precache-manifest.c8baf09ebd6321ff05364dd44911d79f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccf5e07e1fa54bf62f0c2e05e78d9b6",
+    "url": "/nassim-resume/index.html"
+  },
+  {
+    "revision": "7d6d480e516a6b3ef9fb",
+    "url": "/nassim-resume/static/css/main.5c47b20a.chunk.css"
+  },
+  {
+    "revision": "4a342b1e59ed5325294e",
+    "url": "/nassim-resume/static/js/2.eab76a35.chunk.js"
+  },
+  {
+    "revision": "7d6d480e516a6b3ef9fb",
+    "url": "/nassim-resume/static/js/main.d9326220.chunk.js"
+  },
+  {
+    "revision": "48c15d01838b190837a9",
+    "url": "/nassim-resume/static/js/runtime~main.92989014.js"
+  }
+]);
